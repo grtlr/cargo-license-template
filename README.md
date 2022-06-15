95bac0584621e32bca91a3d0166eba5c29005679
Jochen Görtler <jochen.goertler@iota.org>
2022-06-15 09:31:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## cargo-license-template
+
+Recently, `rustfmt` [has removed](https://github.com/rust-lang/rustfmt/commit/79515f17ed4661da864347c90c76c51f9bf86069) the `license_template_path` config option.
+The goal of this subcommand is to restore that functionality.
+
+⚠️ This is still work in progress.
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use clap::Parser;
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use thiserror::Error;
+use walkdir::{DirEntry, WalkDir};
+
+mod license;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+#[clap(
+    bin_name = "cargo license-template",
+    about = "A cargo subcommand to check each Rust file against a license template."
+)]
+pub struct CliArgs {
+    /// The location of the license template.
+    #[clap(value_parser, long)]
+    pub template: String,
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Invalid license in file `{0}`")]
+    InvalidLicense(PathBuf),
+}
+
+fn is_rust_code(entry: &DirEntry) -> bool {
+    entry.path().extension().and_then(|ext| ext.to_str()) == Some("rs")
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // When called by `cargo`, we need to drop the extra `license-template` argument.
+    let args = env::args().enumerate().filter_map(|(i, x)| {
+        if (i, x.as_str()) == (1, "license-template") {
+            None
+        } else {
+            Some(x)
+        }
+    });
+
+    let cli_args = CliArgs::parse_from(args);
+
+    let license_template = license::load_and_compile_template(&cli_args.template)?;
+
+    let metadata = cargo_metadata::MetadataCommand::new().exec()?;
+
+    for entry in WalkDir::new(metadata.workspace_root)
+        .into_iter()
+        .filter_map(|e| {
+            e.ok().and_then(|e| {
+                if !e.path().starts_with(&metadata.target_directory) {
+                    Some(e)
+                } else {
+                    None
+                }
+            })
+        })
+    {
+        if is_rust_code(&entry) {
+            let file_contents = fs::read_to_string(entry.path())?;
+            if !license_template.is_match(&file_contents) {
+                return Err(Box::new(Error::InvalidLicense(entry.path().to_owned())));
+            }
+        }
+    }
+
+    Ok(())
+}
